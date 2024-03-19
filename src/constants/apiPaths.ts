c966d27b6f3cb54882eeddf07c8e40e199fe5a63
Alen Čalaković <33428188+calakovicalen@users.noreply.github.com>
2024-03-19 19:40:50 +0100

    changed apiPaths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://opdxomad5f.execute-api.eu-west-1.amazonaws.com/products",
+  order: "https://opdxomad5f.execute-api.eu-west-1.amazonaws.com/products",
+  import: "https://opdxomad5f.execute-api.eu-west-1.amazonaws.com/products",
+  bff: "https://opdxomad5f.execute-api.eu-west-1.amazonaws.com/products",
+  cart: "https://opdxomad5f.execute-api.eu-west-1.amazonaws.com/products",
 };
 
 export default API_PATHS;
