854ac8358ebf7dc4338ea652fcefbfd4b8227522
Alen Čalaković <33428188+calakovicalen@users.noreply.github.com>
2024-03-26 05:10:34 +0100

    finished task4

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://s5zp2rcl9i.execute-api.eu-west-1.amazonaws.com",
-  order: "https://s5zp2rcl9i.execute-api.eu-west-1.amazonaws.com",
-  import: "https://s5zp2rcl9i.execute-api.eu-west-1.amazonaws.com",
-  bff: "https://s5zp2rcl9i.execute-api.eu-west-1.amazonaws.com",
-  cart: "https://s5zp2rcl9i.execute-api.eu-west-1.amazonaws.com",
+  product: "https://fdxa5jv506.execute-api.eu-west-1.amazonaws.com",
+  order: "https://fdxa5jv506.execute-api.eu-west-1.amazonaws.com",
+  import: "https://fdxa5jv506.execute-api.eu-west-1.amazonaws.com",
+  bff: "https://fdxa5jv506.execute-api.eu-west-1.amazonaws.com",
+  cart: "https://fdxa5jv506.execute-api.eu-west-1.amazonaws.com",
 };
 
 export default API_PATHS;
