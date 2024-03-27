50a17b26219bb188eef1738f6d615b5c78432416
Alen Čalaković <33428188+calakovicalen@users.noreply.github.com>
2024-03-27 21:03:59 +0100

    finished task5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://fdxa5jv506.execute-api.eu-west-1.amazonaws.com",
   order: "https://fdxa5jv506.execute-api.eu-west-1.amazonaws.com",
-  import: "https://fdxa5jv506.execute-api.eu-west-1.amazonaws.com",
+  import: "https://na2z7gn70b.execute-api.eu-west-1.amazonaws.com/import",
   bff: "https://fdxa5jv506.execute-api.eu-west-1.amazonaws.com",
   cart: "https://fdxa5jv506.execute-api.eu-west-1.amazonaws.com",
 };
